ea0325d64720e0745d20769f408beceedeaceea2
kojunggun <ggggg46@naver.com>
2021-03-03 22:44:03 +0900

    Django Post

--- todo/frontend/src/components/App.js
+++ todo/frontend/src/components/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
+
+
 
 export default class App extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+
+        }
     }
 
     render() {
-        return <h1> Testing React Code </h1>;
+        return (
+            <div>
+                <HomePage />
+            </div>
+
+        );
     }
 }
 
 const appDiv = document.getElementById("app");
-render
+render(<App name="kojeonggeun"/>, appDiv);
